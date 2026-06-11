83c929746d0eb6cdf26545984a649693d22b62c2
ShravyaVenishetty-Manuen <shravyavenishetty@manuen.com>
2026-06-11 18:10:04 +0530

    ci: add GitHub Actions workflow and base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Minha_Imports_and_Exports/',
   plugins: [
     react(),
     tailwindcss()
